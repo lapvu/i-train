a1df311ec2e42a773615ce31d363ea5b3c14cb49
github1104 <vipboytk98@gmail.com>
2019-04-07 15:51:29 +0700

    guide

--- iTrainMobile/src/screens/main/settings/index.js
+++ iTrainMobile/src/screens/main/settings/index.js
@@ -1,24 +1,25 @@
 import React from "react";
 import {
   Container,
-  Header,
-  Left,
   Content,
-  Title,
   List,
   ListItem,
   Text
 } from "native-base";
+import colors from "../../../styles/colors"
 import firebase from "react-native-firebase";
 class SettingsScreen extends React.Component {
+  static navigationOptions = {
+    title: "Danh sách chiều đi",
+    headerStyle: {
+      backgroundColor: colors.gradient[0],
+      elevation: 0
+    },
+    headerTintColor: colors.white
+  };
   render() {
     return (
       <Container>
-        <Header>
-          <Left>
-            <Title>Header</Title>
-          </Left>
-        </Header>
         <Content>
           <List>
             <ListItem onPress={() => firebase.auth().signOut()}>
