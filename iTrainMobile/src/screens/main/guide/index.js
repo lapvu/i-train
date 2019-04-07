ec3c75a5037165ebdede3a6293ce601b19359c95
github1104 <vipboytk98@gmail.com>
2019-04-07 18:10:20 +0700

    fix guild

--- iTrainMobile/src/screens/main/guide/index.js
+++ iTrainMobile/src/screens/main/guide/index.js
@@ -17,31 +17,47 @@ export default class GuideScreen extends React.Component {
           </View> 
           <View style={styles.container}>
             <Text style={styles.text}>- Bước 1: Chọn ga đi,ga đến,loại vé,ngày đi, ngày về(vé khứ hồi). </Text>
-            <Image
-              source={require("../../../assets/imgs/guide1.png")}
-              style={styles.img}
-            />
+            <View style={{alignItems:"center"}}>
+              <Image
+                source={require("../../../assets/imgs/guide1.png")}
+                style={styles.img}
+              />
+            </View>
             <Text style={styles.text}>- Bước 2: Sau khi chọn xong ấn tiếp tục để chọn tàu.</Text>
-            <Image
-              source={require("../../../assets/imgs/guide2.png")}
-              style={styles.img}
-            />
+            <View style={{alignItems:"center"}}>
+              <Image
+                source={require("../../../assets/imgs/guide2.png")}
+                style={styles.img}
+              />
+            </View>
             <Text style={styles.text} >- Bước 3: Sau khi chọn xong tàu bạn được chuyển đến danh sách toa tàu.</Text>
-            <Image
-              source={require("../../../assets/imgs/guide3.png")}
-              style={styles.img}
-            />
+            <View style={{alignItems:"center"}}>
+              <Image
+                source={require("../../../assets/imgs/guide3.png")}
+                style={styles.img}
+              />
+            </View>
             <Text style={styles.text} >- Bước 4: Chọn ghế. </Text>
-            <Image
-              source={require("../../../assets/imgs/guide4.png")}
-              style={styles.img}
-            />
+            <View style={{alignItems:"center"}}>
+              <Image
+                source={require("../../../assets/imgs/guide4.png")}
+                style={styles.img}
+              />
+            </View>
             <Text style={styles.text} >- Bước 5: Chọn biểu tượng giỏ hàng để xem giá và đặt vé.</Text>
-            <Image
-              source={require("../../../assets/imgs/guide5.png")}
-              style={styles.img}
-            />
-            <Text style={styles.text} >- Bước 6: </Text>
+            <View style={{alignItems:"center"}}>
+              <Image
+                source={require("../../../assets/imgs/guide5.png")}
+                style={styles.img}
+              />
+            </View>
+            <Text style={styles.text} >- Bước 6: Xác nhận hóa đơn.</Text>
+            <View style={{alignItems:"center"}}>
+              <Image
+                source={require("../../../assets/imgs/guide6.png")}
+                style={styles.img}
+              />
+            </View>
           </View>
         </ScrollView>
       </View>
@@ -52,7 +68,7 @@ const styles = StyleSheet.create({
   container:{
     flex:1,
     marginHorizontal:10,
-    padding:10
+    padding:10,
   },
   title: {
     fontSize:22,
@@ -64,8 +80,9 @@ const styles = StyleSheet.create({
     marginVertical:3
   },
   img: {
-    marginVertical:6,
-    left:30
+    width:200,
+    height:400,
+    resizeMode:'contain'
   } 
 
 })
